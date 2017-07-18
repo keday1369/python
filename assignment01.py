fabb8b62e341683e13cdf78a9399135857ce8f4c
Kara Day <keday1369@gmail.com>
2017-07-17 19:58:39 -0500

    added assignment 1 task

---
+++ assignment01.py
@@ -0,0 +1,16 @@
+
+# Assignment 1:
+#
+#   Update this file to use print a tic-tac-toe board like this:
+#
+#      |   |
+#   ---+---+---
+#      |   |
+#   ---+---+---
+#      |   |
+#
+#   Once created, save this file into git.
+#
+
+
+
