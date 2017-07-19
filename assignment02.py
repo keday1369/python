cbea4cba7a52a69dace721a6a86a89ea969baf26
Kara Day <keday1369@gmail.com>
2017-07-19 16:42:47 -0500

    started assignment2

---
+++ assignment02.py
@@ -0,0 +1,13 @@
+# Assignment 2:
+#
+#   
+#
+#   Once created, save this file into git.
+#   Create a new branch:  git checkout -b assignment02
+#   Complete the assignment python script
+#   Use git add and git commit to save your changes in your branch
+#   Push your branch to github:   git push origin assignment02
+#   Then go to github.com and create a Pull Request for the assignment02 branch
+#
+
+
