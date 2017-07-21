b4d483e14b53ce79bed10fb072aa5632edac96a0
Kara Day <keday1369@gmail.com>
2017-07-21 16:23:33 -0500

    added assignment 1 task

--- assignment02.py
+++ assignment02.py
@@ -5,10 +5,10 @@
 #
 #   1. Create a new branch:  git checkout -b assignment02
 #   2. Use 'git status' to make sure you are on the 'assignment02' branch.
-#   2. Write python code corresponding to the problems below.
-#   3. Use git add and commit to save your changes in your branch.
-#   4. Push your branch to github:   git push origin assignment02
-#   5. Go to github.com and create a 'Pull Request' for the 'assignment02' branch, and assign it to 'mtday'
+#   3. Write python code corresponding to the problems below.
+#   4. Use git add and commit to save your changes in your branch.
+#   5. Push your branch to github:   git push origin assignment02
+#   6. Go to github.com and create a 'Pull Request' for the 'assignment02' branch, and assign it to 'mtday'
 #
 
 # Example Problem: Create a variable called "z" with an integer value of:   10
@@ -16,37 +16,38 @@
 z = 10
 
 # Problem 1: Create a variable called "a" with an integer value of:   -5
-
+a = -5
 
 # Problem 2: Create a variable called "b" with a floating point value of:   3.5
-
+b = 3.5
 
 # Problem 2: Create a variable called "c" with a string value of:   Hello
-
+c = "Hello"
 
 # Problem 3: Create a variable called "d" with a string value of:   She said, "Hello"
-
+d = 'She said, "Hello"'
 
 # Problem 4: Create a variable called "e" with a string value of:   He replied, "I'm fine, thank you!"
-
+e = 'He replied, "I\'m fine, thank you!"'
 
 # Problem 5: Create a variable called "f" that is the variable "a" divided by "b" using floating point division.
-
+f = a / b
 
 # Problem 6: Create a variable called "g" that is the variable "a" divided by "b" using integer division.
-
+g = a // b
 
 # Problem 7: Create a variable called "h" that contains variables "c" and "d" concatenated together.
-
+h = c + d
 
 # Problem 8: Create a variable called "j" that is a list containing variables "a", "b", and "c".
+j = [a, b, c]
 
+# Problem 9: Create a variable called "k" set to the first value in the list "j", using square brackets.
+k = j[0]
 
-# Problem 9: Create a variable called "k" set to the first value in the list "i", using square brackets.
-
-
-# Problem 10: Create a variable called "m" set to the last value in the list "i", using square brackets with a
+# Problem 10: Create a variable called "m" set to the last value in the list "j", using square brackets with a
 # negative index.
+m = j[-1]
 
 # Problem 11: Create a variable called "n" set to the sum of the first and second elements from list "j".
-
+n = j[0] + j[1]
