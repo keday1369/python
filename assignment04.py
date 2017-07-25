6e1b847cfc3a1a3731aec78cb209c5facf9a6069
mday <mday@eitccorp.com>
2017-07-24 22:31:14 -0500

    added assignment 4

---
+++ assignment04.py
@@ -0,0 +1,63 @@
+# Assignment 4:
+#
+#   Problems in this assignment are based on the information from this page:
+#       http://www.python-course.eu/python3_list_manipulation.php
+#
+#   1. Create a new branch in PyCharm with name assignment04
+#   2. Write python code corresponding to the problems below.
+#   3. Use PyCharm to add, commit, and push your changes into your git branch.
+#   4. Go to github.com and create a 'Pull Request' for the 'assignment04' branch, and assign it to 'mtday'
+#
+
+# Problem 1: Create a variable called "letters" that is an empty list.
+
+
+# Problem 2: Use the list functions (pop, append, extend, insert, remove, index) to add the string "B" to your list,
+# and print the result. The output should be:   ['B']
+
+
+# Problem 3: Use the list functions (pop, append, extend, insert, remove, index) to add the string "A" to the beginning
+# of the list and print the result. The output should be:   ['A', 'B']
+
+
+# Problem 4: Use the list functions (pop, append, extend, insert, remove, index) to add the string "C" to the end
+# of the list and print the result. The output should be:   ['A', 'B', 'C']
+
+
+# Problem 5: Use the list functions (pop, append, extend, insert, remove, index) to add the string "Z" in between
+# "A" and "B" in the list and then print the list. The output should be:   ['A', 'Z', 'B', 'C']
+
+
+# Problem 6: Use the list functions (pop, append, extend, insert, remove, index) to add the strings "D", "E", and "F"
+# to the end of the list using a single function call, then print the list. The output should be:
+#    ['A', 'Z', 'B', 'C', 'D', 'E', 'F']
+
+
+# Problem 7: Use the list functions (pop, append, extend, insert, remove, index) to remove the string "Z" from the
+# list, then print the list. The output should be:   ['A', 'B', 'C', 'D', 'E', 'F']
+
+
+# Problem 8: Use the list functions (pop, append, extend, insert, remove, index) to move the last element in the list
+# to the beginning of the list, then print the list. The output should be:   ['F', 'A', 'B', 'C', 'D', 'E']
+
+
+# Problem 9: Print out the length of the list. The output should be:   6
+
+
+# Problem 10: Print out the first element in the list. The output should be:   A
+
+
+# Problem 11: The python documentation (https://docs.python.org/3/tutorial/datastructures.html) indicates that lists
+# have a "count" function that returns how many times an element appears in the list. Call the "count" function on
+# the list to find out how many "A" values exist in the list and print the result. The output should be:   1
+
+
+# Problem 12: The python documentation (https://docs.python.org/3/tutorial/datastructures.html) indicates that lists
+# have a "sort" function that orders the values in the list. Call the "sort" function on the list, then print the list.
+# The output should be:   ['A', 'B', 'C', 'D', 'E']
+
+
+# Problem 13: The python documentation (https://docs.python.org/3/tutorial/datastructures.html) indicates that lists
+# have a "clear" function that removes all the values from the list. Call the "clear" function on the list, then print
+# the list. The output should be:   []
+
