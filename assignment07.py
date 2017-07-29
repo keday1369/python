c4c6b23431ef3c94d9806ff409c02d780d37b0fe
mday <mday@eitccorp.com>
2017-07-28 22:26:40 -0500

    added assignments 5, 6, and 7

---
+++ assignment07.py
@@ -0,0 +1,40 @@
+# Assignment 7:
+#
+#   Problems in this assignment are based on the information from this page:
+#       http://www.python-course.eu/python3_conditional_statements.php
+#
+#   1. Create a new branch in PyCharm with name assignment07
+#   2. Write python code corresponding to the problems below.
+#   3. Use PyCharm to add, commit, and push your changes into your git branch.
+#   4. Go to github.com and create a 'Pull Request' for the 'assignment07' branch, and assign it to 'mtday'
+#
+
+# Problem 1: Create a boolean variable called "a" with a true value.
+
+
+# Problem 2: Create a boolean variable called "b" with a false value.
+
+
+# Problem 3: Write an "if" statement that prints "Yes" if "a" is true. The output should be:   Yes
+
+
+# Problem 4: Write an "if" statement that prints "Yes" if either "a" or "b" are true. The output should be:   Yes
+
+
+# Problem 5: Write an "if" statement that prints "Yes" if "a" is true and "b" is not true. The output should be:   Yes
+
+
+# Problem 6: Write an "if/else" statement that prints "Yes" if "b" is true and "No" if "b" is false.
+# The output should be:   No
+
+
+# Problem 7: Create a variable called "num" with a value of 10.
+
+
+# Problem 8: Write an "if/elif/else" statement that prints "Small" if "num" is less than 5, "Medium" if "num" is 5
+# through 10, and "Large" if "num" is greater than 10. The output should be:    Medium
+
+
+# Problem 9: Create a variable "max" initialized to -1 if "num" is less than 100 and 1 if "num" is greater than or
+# equal to 100.
+
