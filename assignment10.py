a9eb49578d63ab1cde7af6342dc12f80493a1ade
mday <mday@eitccorp.com>
2017-08-02 21:52:22 -0500

    added assignment 10

---
+++ assignment10.py
@@ -0,0 +1,11 @@
+#
+# Assignment 10:
+#
+# Solve this problem from https://projecteuler.net/problem=4:
+#
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers
+# is 9009 = 91 x 99.
+#
+# Find the largest palindrome made from the product of two 3-digit numbers.
+#
+
