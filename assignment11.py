a318f3b3940da50145d6566831b92654dec3dcaf
Kara Day <keday1369@gmail.com>
2017-08-04 17:44:12 -0500

    added assignment 11 task

--- assignment11.py
+++ assignment11.py
@@ -10,3 +10,12 @@
 #    6 | 7 | 8
 #
 
+row = 1
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+number = 0
+while row < 4:
+    print(" " + str(numbers[number]) + " | " + str(numbers[number + 1]) + " | " + str(numbers[number + 2]))
+    if row != 3:
+        print("---+---+---")
+    number += 3
+    row += 1
